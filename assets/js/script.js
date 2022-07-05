14711e2f66d0cb038c701de39b0755bf32dd3d04
Brian King <bnk5532@gmail.com>
2022-07-05 14:56:25 -0400

    notes added to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,14 +15,15 @@ var availTimeSlots = [
 
 ];
 
+//generates dynamic rows of time slots when called.
 getTimeBlocks();
 
 function getTimeBlocks() {
   timeBlocks.innerHTML = "";
-
+// loop iterates for the length of times available in array
   for (var i = 0; i < availTimeSlots.length; i++) {
     var hourBlock = availTimeSlots[i];
-
+//built dynamic rows which read from availTimeSlots array
     var row = document.createElement("div");
     row.classList.add("row");
     timeBlocks.appendChild(row);
@@ -31,13 +32,13 @@ function getTimeBlocks() {
     hour.innerHTML = hourBlock;
     hour.classList.add("hour");
     row.appendChild(hour);
-
+//textarea created to capture appointment details and make accessible to local storage.
     var textarea = document.createElement("textarea");
     textarea.placeholder = "Enter your text here";
     textarea.setAttribute("class", "description");
     textarea.setAttribute("id", i);
     row.appendChild(textarea);
-
+//save btn will record values and reserve the time slot.
     var saveBtn = document.createElement("button");
     saveBtn.textContent = "Save";
     saveBtn.classList.add("saveBtn");
